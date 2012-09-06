2397f2ed5cad1e1afa17f37ff9b20021f6608e85
Dag Olav Prestegarden <dagolap@stud.ntnu.no>
2012-09-06 15:07:25 +0200

    Registering mangekamp models in admin interface

---
+++ mangekampentrondheim/mangekamp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from mangekamp.models import Season, Event, Participation, Score
+
+admin.site.register(Season)
+admin.site.register(Event)
+admin.site.register(Participation)
+admin.site.register(Score)
