5d36dd334b45c384835c1f9074bf742d891f4f56
dagolap <Dag Olav Prestegarden dagolav@prestegarden.com>
2012-09-11 12:59:07 +0200

    Adding image management et. al.
    * Fixing a lot of settings to media paths etc.
    * Adding media folder for uploaded images.
    * Setting up static rendering of media when in debug

--- mangekampentrondheim/mangekampentrondheim/urls.py
+++ mangekampentrondheim/mangekampentrondheim/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from filebrowser.sites import site
 
@@ -19,3 +21,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+
+    urlpatterns += patterns('',
+            url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                'document_root': settings.MEDIA_ROOT,
+                'show_indexes': True
+                })
+            )
