132e9dd46aae7b35425f45eeb4c55c8761dc4f1c
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-06-03 10:34:53 +0200

    Changing DB password so we can overwrite it in deployment script

--- mangekampentrondheim/mangekampentrondheim/settings.py
+++ mangekampentrondheim/mangekampentrondheim/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mangekampen',                      # Or path to database file if using sqlite3.
         'USER': 'mangekampen',                      # Not used with sqlite3.
-        'PASSWORD': 'Aevie0up',                  # Not used with sqlite3.
+        'PASSWORD': 'db_password',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
